71c802b7eced6b019adc9d6dd446a047c63b9548
udemegane <cappuccino1204.ck@gmail.com>
2022-07-08 10:30:14 +0900

    階層完全一致のみサポートするボーン抽象構造を追加

--- src/abstructBoneHierarchy.ts
+++ src/abstructBoneHierarchy.ts
@@ -7,8 +7,9 @@
  * べーすとなるスケルトンを受け取る
  * 渡されたスケルトンが一致するか判定
  */
+/* WIP!!!!!!!!!!!!!!!!!!!!!!! */
 
-import { Bone, Skeleton } from "@babylonjs/core";
+import { Bone, IAnimatable, Skeleton, Animation } from "@babylonjs/core";
 import { assertIsDefined } from "./main";
 
 type HierarchicalAnimationNode = {
@@ -27,6 +28,8 @@ type HierarchicalBoneDataNode = {
 
 export type BoneHierarchyBase = {
   name: string;
+  length: number;
+  hash: string;
   boneHierarchy: HierarchicalBoneDataNode;
 };
 
@@ -48,38 +51,116 @@ export const makeBoneHierarchyBase = (
     },
     children: [],
   };
-  const constructHierarchy = (bone: Bone, node: HierarchicalBoneDataNode) => {
+  const constructHierarchy = (
+    bone: Bone,
+    node: HierarchicalBoneDataNode
+  ): [HierarchicalBoneDataNode, string] => {
     node.data = {
       id: bone.getIndex(),
       name: bone.name,
     };
     if (bone.children.length === 0) {
       node.children = undefined;
-      return node;
+      return [node, "()"];
     } else {
-      node.children = bone.children.map((cbone) =>
-        constructHierarchy(cbone, node)
-      );
+      const chashArray: string[] = [];
+      const cNodeArray: HierarchicalBoneDataNode[] = [];
+      const result = bone.children.map((cbone) => {
+        return constructHierarchy(cbone, node);
+      });
+      result.forEach((data) => {
+        cNodeArray.push(data[0]);
+        chashArray.push(data[1]);
+      });
+      node.children = cNodeArray;
+      return [node, `(${chashArray.sort()})`];
     }
-    return node;
   };
+  const [boneHierarchy, hash] = constructHierarchy(rootBone, rootNode);
   return {
     name: name,
-    boneHierarchy: constructHierarchy(rootBone, rootNode),
+    length: skeleton.bones.length,
+    hash: hash,
+    boneHierarchy: boneHierarchy,
   };
 };
 
-type IsVailedSkeleton = (
+const getSkeletonHierarchyHash = (skeleton: Skeleton): string => {
+  const constructHash = (bone: Bone): string => {
+    if (bone.children.length === 0) {
+      return "()";
+    } else {
+      const chashArray = bone.children.map((cbone) => constructHash(cbone));
+      return `(${chashArray.sort()})`;
+    }
+  };
+  const rootBone = skeleton.bones[0];
+  assertIsDefined(rootBone);
+  return constructHash(rootBone);
+};
+
+type IsValiedSkeleton = (
   skeleton: Skeleton,
   boneHierarchy: BoneHierarchyBase
 ) => boolean;
+const isValiedSkeleton: IsValiedSkeleton = (
+  skeleton: Skeleton,
+  boneHierarchy: BoneHierarchyBase
+) => {
+  const skeletonHash = getSkeletonHierarchyHash(skeleton);
+  const [mainTree, subTree] = (() => {
+    if (skeleton.bones.length > boneHierarchy.length)
+      return [skeletonHash, boneHierarchy.hash];
+    else return [boneHierarchy.hash, skeletonHash];
+  })();
+  const compareHierarchy = (mainTree: string, subTree: string): boolean => {
+    // 部分一致を許容するとめんどくさすぎるのでとりあえず完全一致で妥協.
+    return mainTree === subTree;
+  };
+  return compareHierarchy(mainTree, subTree);
+};
 
 type MakeSkeletonAnimationClip = (
   skeleton: Skeleton,
   boneHierarchy: BoneHierarchyBase
 ) => SkeletonAnimationClip;
-/*
-const makeSkeletonAnimationClip: MakeSkeletonAnimationClip = (
-  skeleton: Skeleton
-): SkeletonAnimationClip => {};
-*/
+
+export const makeSkeletonAnimationClip: MakeSkeletonAnimationClip = (
+  skeleton,
+  boneHierarchy
+) => {
+  if (!isValiedSkeleton(skeleton, boneHierarchy)) {
+    throw new Error(`スケルトンとベースボーンヒエラルキーが一致しません`);
+  }
+  const constructAnimation = (
+    bone: Bone,
+    animatables: IAnimatable[]
+  ): HierarchicalAnimationNode => {
+    const id = bone.getIndex();
+    const anims = animatables[id]?.animations;
+    assertIsDefined(anims);
+    const anim = anims[0];
+    assertIsDefined(anim);
+    if (bone.children.length === 0) {
+      return {
+        id: id,
+        animation: anim,
+        children: undefined,
+      };
+    } else {
+      return {
+        id: id,
+        animation: anim,
+        children: bone
+          .getChildren()
+          .map((cbone) => constructAnimation(cbone, animatables)),
+      };
+    }
+  };
+  const rootBone = skeleton.bones[0];
+  assertIsDefined(rootBone);
+  return {
+    skeletonAnimation: constructAnimation(rootBone, skeleton.getAnimatables()),
+    baseHierarchy: boneHierarchy,
+  };
+};
