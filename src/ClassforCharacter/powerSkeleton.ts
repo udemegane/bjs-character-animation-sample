e38472443dc2acd3a7004fb71a767c90339e041d
udemegane <cappuccino1204.ck@gmail.com>
2022-11-16 20:19:39 +0900

    ==の代わりに===

--- src/ClassforCharacter/powerSkeleton.ts
+++ src/ClassforCharacter/powerSkeleton.ts
@@ -10,7 +10,7 @@ export type SkeletonHash = {
 const uuid = () =>
   "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, function (c) {
     const r = (Math.random() * 16) | 0;
-    const v = c == "x" ? r : (r & 0x3) | 0x8;
+    const v = c === "x" ? r : (r & 0x3) | 0x8;
     return v.toString(16);
   });
 
